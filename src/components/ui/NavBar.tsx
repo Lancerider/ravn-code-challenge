accd1c63ba0e1cbb46ebbfd6d5d74d6f3ffef5b8
Jorge Luis Palacios <jorgepalacioscl@gmail.com>
2022-05-27 13:09:19 -0400

    :sparkles: NavBar dynamic name

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,11 +1,24 @@
+import { useContext } from 'react';
+
+import AppContext from '../../context/AppContext';
+
+import { AppContextType } from '../../interfaces/AppContextType';
+
 import style from './navbar.module.scss';
 
-const NavBar = () => (
-  <nav className={ style.navbar }>
-    <div className={ style.title }>
-      People of Star Wars
-    </div>
-  </nav>
-)
+const NavBar = () => {
+  const { characterSelected, isMobile } = useContext(AppContext) as AppContextType;
+
+  return (
+    <nav className={ style.navbar }>
+      <div className={ style.title }>
+        { isMobile
+          ? characterSelected ? characterSelected.name : 'People of Star Wars'
+          : 'Ravn Star Wars Registry'
+        }
+      </div>
+    </nav>
+  )
+}
 
 export default NavBar;
