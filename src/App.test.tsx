681c5f5c9ad45aec1f1a189cc8467d645a1cade3
Jorge Luis Palacios <jorgepalacioscl@gmail.com>
2022-06-03 10:43:35 -0400

    🧪 Adding main test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders the header title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerTitle = screen.getByText(/People of Star Wars|Ravn Star Wars Registry/i);
+  expect(headerTitle).toBeInTheDocument();
 });
